e985b44e0059d1f2d0072bb1eaa2e3439f318cf6
Naraku777 <442815900@qq.com>
2017-06-17 18:10:00 +0800

    Could config the favicon

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -28,6 +28,7 @@ module.exports = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
+      favicon: require('../src/config').favicon || false,
       inject: true
     }),
     new FriendlyErrorsPlugin()
